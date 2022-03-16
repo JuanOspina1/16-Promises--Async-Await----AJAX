2970621c9c354bbc0e4349de0cac88019db28930
Juan Ospina <juanospina11@gmail.com>
2022-03-16 17:20:55 -0400

    completed challenge 1

--- starter/script.js
+++ starter/script.js
@@ -3,9 +3,6 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
-///////////////////////////////////////
-// Old way of doing this
-
 const renderCountry = function (data, className = '') {
   const html = `        
     <article class="country ${className}">
@@ -33,6 +30,16 @@ const renderError = function (msg) {
   countriesContainer.insertAdjacentText('beforeend', msg);
   // countriesContainer.style.opacity = 1;
 };
+
+const getJSON = function (url, errorMsg = 'Something went wrong') {
+  return fetch(url).then(response => {
+    if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+
+    return response.json();
+  });
+};
+///////////////////////////////////////
+// Old way of doing this
 /*
 const getCountryAndNeighbor = function (country) {
   // AJAX Call country 1
@@ -74,14 +81,6 @@ getCountryAndNeighbor('usa');
 // const request = new XMLHttpRequest();
 //   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
 //   request.send();
-
-const getJSON = function (url, errorMsg = 'Something went wrong') {
-  return fetch(url).then(response => {
-    if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
-
-    return response.json();
-  });
-};
 
 /////////////////////
 // Refactoring this section
@@ -130,13 +129,14 @@ const getJSON = function (url, errorMsg = 'Something went wrong') {
 //       return null
 //     };
 
+//// Cannot use this for challenge # 1
 const getCountryData = function (country) {
   // Country 1
 
   getJSON(`https://restcountries.com/v3.1/name/${country}`, 'Country not found')
     .then(data => {
       renderCountry(data[0]);
-
+      console.log(data);
       const neighbour = data[0]?.borders?.[0];
 
       if (!neighbour) throw new Error('No Neighbour found');
@@ -147,7 +147,7 @@ const getCountryData = function (country) {
         'Country not found'
       );
     })
-    .then(data => renderCountry(...data, 'neighbour'))
+    .then(data => renderCountry(...data))
     .catch(err => {
       console.error(`${err} ***`);
       renderError(`Something went wrong: ${err.message}`);
@@ -157,8 +157,74 @@ const getCountryData = function (country) {
     });
 };
 
-btn.addEventListener('click', function () {
-  getCountryData('portugal');
-});
+// btn.addEventListener('click', function () {
+//   getCountryData('portugal');
+// });
 
-getCountryData('australia');
+// getCountryData('austria');
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+In this challenge you will build a function 'whereAmI' which renders a country ONLY based on GPS coordinates. For that, you will use a second API to geocode coordinates.
+
+Here are your tasks:
+
+PART 1
+1. Create a function 'whereAmI' which takes as inputs a latitude value (lat) and a longitude value (lng) (these are GPS coordinates, examples are below).
+2. Do 'reverse geocoding' of the provided coordinates. Reverse geocoding means to convert coordinates to a meaningful location, like a city and country name. Use this API to do reverse geocoding: https://geocode.xyz/api.
+The AJAX call will be done to a URL with this format: https://geocode.xyz/52.508,13.381?geoit=json. Use the fetch API and promises to get the data. Do NOT use the getJSON function we created, that is cheating 😉
+3. Once you have the data, take a look at it in the console to see all the attributes that you recieved about the provided location. Then, using this data, log a messsage like this to the console: 'You are in Berlin, Germany'
+4. Chain a .catch method to the end of the promise chain and log errors to the console
+5. This API allows you to make only 3 requests per second. If you reload fast, you will get this error with code 403. This is an error with the request. Remember, fetch() does NOT reject the promise in this case. So create an error to reject the promise yourself, with a meaningful error message.
+
+PART 2
+6. Now it's time to use the received data to render a country. So take the relevant attribute from the geocoding API result, and plug it into the countries API that we have been using.
+7. Render the country and catch any errors, just like we have done in the last lecture (you can even copy this code, no need to type the same code)
+
+TEST COORDINATES 1: 52.508, 13.381 (Latitude, Longitude)
+TEST COORDINATES 2: 19.037, 72.873
+TEST COORDINATES 2: -33.933, 18.474
+
+GOOD LUCK 😀
+*/
+// 1. Create a function 'whereAmI' which takes as inputs a latitude value (lat) and a longitude value (lng) (these are GPS coordinates, examples are below).
+
+const whereamI = function (lat, lng) {
+  fetch(
+    `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}&localityLanguage=en`
+  )
+    .then(country => {
+      console.log(country);
+      if (!country.ok)
+        throw new Error(`Problem with geocoding ${country.status}`);
+      return country.json();
+    })
+    .then(data => {
+      console.log(data);
+      const countryName = data.countryName;
+      const cityName = data.city;
+      // 3. Once you have the data, take a look at it in the console to see all the attributes that you recieved about the provided location. Then, using this data, log a messsage like this to the console: 'You are in Berlin, Germany'
+      console.log(`You are in ${cityName} ${countryName}`);
+
+      return fetch(`https://restcountries.com/v3.1/name/${countryName}`);
+    })
+    .then(res => {
+      if (!res.ok) throw new Error(`Country not found (${res.status})`);
+      console.log(res);
+      return res.json();
+    })
+    .then(data1 => renderCountry(...data1))
+    .catch(err => {
+      console.error(`${err} *`);
+      renderError(`Something went wrong: ${err.message}`);
+    })
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
+    });
+};
+
+whereamI(52.508, 13.381);
+whereamI(19.037, 72.873);
+whereamI(-33.933, 18.474);
